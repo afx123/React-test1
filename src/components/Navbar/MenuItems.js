956adc2dc975ce792506f44a647ef5286924e41d
Sebastian <sebastian@dango.digital>
2020-12-28 23:05:47 -0300

    Navbar agregado
    Navbar con el tutorial online

---
+++ src/components/Navbar/MenuItems.js
@@ -0,0 +1,23 @@
+export const MenuItems = [
+  {
+    title: 'Inicio',
+    url: '#',
+    cName: 'nav-links',
+  },  
+  {
+    title: 'Segundo',
+    url: '#',
+    cName: 'nav-links',
+  },
+  {
+    title: 'Tercero',
+    url: '#',
+    cName: 'nav-links',
+  },
+  {
+    title: 'Inicio Mobile',
+    url: '#',
+    cName: 'nav-links-mobile',
+  },
+
+]
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import { MenuItems } from "./MenuItems"
+import './Navbar.css'
+
+class Navbar extends Component {
+  state = { clicked: false }
+
+  handleClick = () => {
+    this.setState({ clicked: !this.state.clicked })
+  }
+
+  render() {
+    return(
+      <nav className="NavbarItems">
+        <h1 className="navbar-logo"><i className="fa fa-beer"></i> React</h1>
+        <div className="menu-icon" onClick={this.handelClick}>
+          <i className={this.state.clicked ? 'fa fa-beer' : 'fa fa-bars'}></i>
+        </div>
+        <ul className={this.state.clicked ? 'nav-menu active' : 'nav-menu'}>
+          {MenuItems.map((item, index) => {
+            return (
+              <li key={index}>
+                <a className={item.cName} href={item.url}>
+                  {item.title}
+                </a>
+              </li>
+            )
+          })}
+        </ul>
+      </nav>
+    )
+  }
+}
+
+export default Navbar
